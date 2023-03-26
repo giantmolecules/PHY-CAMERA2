37d6fe18e47f20f73c1bfe55e37f8bc86df6ab28
giantmolecules <giantmolecules@gmail.com>
2023-03-26 13:47:58 -0500

    fixed still-script with new path

--- still-script.sh
+++ still-script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo /usr/bin/python /home/balogh/Documents/light-control.py --on
-/usr/bin/python /home/balogh/Documents/take-still.py
-/usr/bin/python /home/balogh/Documents/get-conditions.py
-sudo /usr/bin/python /home/balogh/Documents/light-control.py --off
+sudo /usr/bin/python /home/balogh/Documents/PHY-CAMERA2/light-control.py --on
+/usr/bin/python /home/balogh/Documents/PHY-CAMERA2/take-still.py
+/usr/bin/python /home/balogh/Documents/PHY-CAMERA2/get-conditions.py
+sudo /usr/bin/python /home/balogh/Documents/PHY-CAMERA2/light-control.py --off
